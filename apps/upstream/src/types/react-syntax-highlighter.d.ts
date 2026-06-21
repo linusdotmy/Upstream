23fa4987265406cd86bb5a39d0f7510c60bf75f0
linuskang <linus@kang.id.au>
2026-06-21 11:35:01 +1000

    really hate ci

---
+++ apps/upstream/src/types/react-syntax-highlighter.d.ts
@@ -0,0 +1,24 @@
+declare module "react-syntax-highlighter" {
+    import * as React from "react"
+
+    interface SyntaxHighlighterProps {
+        language?: string
+        style?: Record<string, React.CSSProperties>
+        children: string
+        className?: string
+        PreTag?: keyof JSX.IntrinsicElements | React.ComponentType<any>
+        [key: string]: any
+    }
+
+    export const Prism: React.FC<SyntaxHighlighterProps>
+    export const Light: React.FC<SyntaxHighlighterProps>
+    export default Prism
+}
+
+declare module "react-syntax-highlighter/dist/esm/styles/prism" {
+    export const vscDarkPlus: Record<string, React.CSSProperties>
+}
+
+declare module "react-syntax-highlighter/dist/cjs/styles/prism" {
+    export const vscDarkPlus: Record<string, React.CSSProperties>
+}
